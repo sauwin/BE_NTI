34abb595afe528d4e79eb9b41edc68c96eff35a3
Maksym Druziuk <druziukmaksym@gmail.com>
2026-04-28 00:37:54 +0200

    Create teams team_members migration

---
+++ database/migrations/2026_04_27_221823_create_teams_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('teams', function (Blueprint $table) {
+            $table->id();
+            $table->string("name")
+                ->unique();
+            $table->foreignId("leader_user_id")
+                ->constrained('users')
+                ->onDelete('set null')
+                ->nullable();
+            $table->text("description")
+                ->nullable();
+            $table->timestamps();
+            $table->index('leader_user_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('teams');
+    }
+};
---
+++ database/migrations/2026_04_27_222641_create_team_members_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('team_members', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId("team_id")
+                ->constrained("teams")
+                ->onDelete('cascade');
+            $table->foreignId("user_id")
+                ->constrained("users")
+                ->onDelete('cascade');
+            $table->date("joined_at");
+            $table->date("left_at")
+                ->nullable();
+            $table->timestamps();
+            $table->unique(['team_id', 'user_id']);
+            $table->index('team_id');
+            $table->index('user_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('team_members');
+    }
+};
